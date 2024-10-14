063b205dd9b6f4e867324f735885d6d7207eea3c
Deepanshu <indiaboybest@gmail.com>
2024-10-14 23:33:48 +0530

    Stop Watch complete

---
+++ script.js
@@ -0,0 +1,94 @@
+let startBtn = document.querySelector('.start');
+let resetBtn = document.querySelector('.reset');
+let minute = document.querySelector('.minute');
+let second = document.querySelector('.second');
+let one_hunderth_second = document.querySelector('.one_hunderth_second');
+let laps = document.querySelector('.laps')
+let lapbox = document.querySelector('.lapbox')
+
+let minute_now = 0;
+let second_now = 0;
+let miliSecond_now = 0;
+let inervalID;
+
+
+function updateTime() {
+    if (miliSecond_now == 100) {
+        second_now++;
+        miliSecond_now = 0;
+    }
+    if (second_now == 60) {
+        minute_now++;
+        second_now = 0;
+    }
+    second.innerHTML = second_now.toString().padStart(2, 0);
+    minute.innerHTML = minute_now.toString().padStart(2, 0);
+
+    miliSecond_now++;
+    one_hunderth_second.innerHTML = miliSecond_now.toString().padStart(2, 0);
+}
+
+function start_watch() {
+    inervalID = setInterval(updateTime, 10);
+}
+
+function change_value() {
+    if (startBtn.innerHTML === "Start") {
+        startBtn.innerHTML = "Stop";
+        resetBtn.innerHTML = "Lap";
+        startBtn.style.cssText = "Background-color: red;";
+        resetBtn.style.cssText = "Background-color: blue";
+        start_watch();
+    } else {
+        clearInterval(inervalID)
+        startBtn.style.cssText = "Background-color: #1ffb1f;";
+        resetBtn.style.cssText = "Background-color: red";
+        startBtn.innerHTML = "Start";
+        resetBtn.innerHTML = "Reset";
+    }
+}
+
+let lapcount = 1;
+let minute_lap = 0;
+let second_lap = 0;
+let miliSecond_lap = 0;
+
+
+function laped() {
+    lapbox.classList.add('lapbox_open')
+    let min = (minute_now - minute_lap).toString().padStart(2, 0);
+    minute_lap = minute_now
+    let sec = (second_now - second_lap).toString().padStart(2, 0);
+    second_lap = second_now
+    let mili = (miliSecond_now - miliSecond_lap).toString().padStart(2, 0);
+    miliSecond_lap = miliSecond_now
+    
+    let current_lap = document.createElement('div')
+    current_lap.classList.add('lap');
+    current_lap.innerHTML = `
+    <h3>${lapcount}.</h3>
+    <h4>${min}:${sec}:${mili}</h4>
+    <h4>${minute.innerHTML}:${second.innerHTML}:${one_hunderth_second.innerHTML}</h4>`
+    laps.prepend(current_lap);
+    lapcount++;
+}
+
+function reset_value() {
+    if (resetBtn.innerHTML == "Reset") {
+        minute_now = 0;
+        second_now = 0;
+        miliSecond_now = 0;
+        lapcount = 1;
+        minute_lap = 0;
+        second_lap = 0;
+        miliSecond_lap = 0
+        lapbox.classList.remove('lapbox_open')
+        laps.innerHTML = ""
+        second.innerHTML = second_now.toString().padStart(2, 0);
+        minute.innerHTML = minute_now.toString().padStart(2, 0);
+        one_hunderth_second.innerHTML = miliSecond_now.toString().padStart(2, 0);
+        resetBtn.style.cssText = "Background-color: black";
+    } else {
+        laped();
+    }
+}
